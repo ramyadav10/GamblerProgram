4e3deea91fb9f259c4fd3bc7b43f9abcdbcc39c9
Ram Yadav <ramyadav.now@gmail.com>
2020-07-31 14:31:19 +0530

    Added Gambler Program: UserCase-01

--- gamblerUC.sh
+++ gamblerUC.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-<<<<<<< HEAD
 #UC-01:-> Gambler would start with a stake of $100 every day and can bet $1 each game
-=======
-#UC-01:-> Gambler would start with a stake of $100 every day and can bet $1 eac>
->>>>>>> 8bc70584ebdb53c5197e74b07e2d555421819118
 
 #Intialising variables
 Stake=100
